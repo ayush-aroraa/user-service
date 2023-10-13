fecdcf6f954b4abac70a9ff2c79873f922ab21ff
rohin4u <rohinmaleyawr@gmail.com>
2023-10-13 10:17:37 +0530

    added swagger with tags

--- src/main/java/com/transferagent/userservice/controller/UserDetailController.java
+++ src/main/java/com/transferagent/userservice/controller/UserDetailController.java
@@ -2,6 +2,10 @@ package com.transferagent.userservice.controller;
 
 import com.transferagent.userservice.entities.UserDetails;
 import com.transferagent.userservice.service.UserDetailService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,11 +15,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api/userdetails")
+@Tag(name="UserDetails", description = "User Details Services API's")
 public class UserDetailController {
 
     private final UserDetailService userDetailService;
@@ -27,7 +32,11 @@ public class UserDetailController {
 
     //sign up api
     @PostMapping("/signup")
-   
+    @Operation(
+    		summary = "Add a user providing all user details",
+    		description = "Add a User with all Properties",
+    		tags = "signup"
+    		)
     public ResponseEntity<Object> addUser(@RequestBody UserDetails userDetails){
         UserDetails user=null;
         try {
@@ -40,7 +49,11 @@ public class UserDetailController {
     }
     
     @GetMapping("/signIn/{id}")
-
+    @Operation(
+    		summary = "Get a Single User Details",
+    		description = "Get a Single User Details",
+    		tags = "signin"
+    		)
     public ResponseEntity<UserDetails> getUser(@PathVariable String id){
     	
     	Optional<UserDetails> fetchUserDetail = userDetailService.fetchUserDetail(id);
--- src/main/java/com/transferagent/userservice/entities/UserDetails.java
+++ src/main/java/com/transferagent/userservice/entities/UserDetails.java
@@ -1,23 +1,30 @@
 package com.transferagent.userservice.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
-@Table(name = "UserDetailsPII")
+@Table(name = "userDetailsPII")
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Schema(description= "User Details Schema")
 public class UserDetails {
     @Id
+    @Schema(description = "User Id in String", example = "AB-123")
     private String objectId;
+    @Schema(description = "User First Name", example="Alexender")
     private String firstName;
+    @Schema(description = "User Middle Name", example = "Brown")
     private String middleName;
+    @Schema(description = "User Last Name", example = "2nd")
     private String lastName;
+    @Schema(description = "User Email Id", example = "abc@xyz.com")
     private String emailId;
 }
